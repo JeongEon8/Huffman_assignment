e3d595b2aa2a0aae11fb43cdc5bba4c17d4200bb
JeongEon8 <113885866+JeongEon8@users.noreply.github.com>
2023-05-16 23:49:57 +0900

    0516 Update(main, class)
    main.cpp Update
    class.h Update

--- class.h
+++ class.h
@@ -1,3 +1,18 @@
 #pragma once
-#include <stdio.h>
-#include <stdlib.h>
+#include <iostream>
+#include <cstring>
+
+class Node {
+public:
+	int weight;
+	int* nxt_ptr;
+	int* prev_ptr;
+};
+
+// 입력 받은 문자열의 문자 빈도수를 반환하는 함수
+void frequency(char input[10000]) {
+
+}
+
+
+// 빈도수를 내림차순으로 바꾸는 함수
--- main.cpp
+++ main.cpp
@@ -3,6 +3,11 @@
 using namespace std;
 
 int main(void) {
+	char input[10000];
+
+	printf("입력(영어, 특수문자만 입력해주세요.) >> ");
+	cin >> input;
+	cout << input;
 
 	return 0;
 }
