897d2832f1424dcacf98daa881c9c2d28551cf16
JeongEon8 <113885866+JeongEon8@users.noreply.github.com>
2023-05-19 23:57:07 +0900

    0519 Update(frequency 세기 및 노드 생성)
    frequency 세기 및 노드 생성

--- class.h
+++ class.h
@@ -1,18 +1,107 @@
+// 2023 알고리즘(박병준 교수님)
+// 과제명 : Huffman 코드를 이용한 파일압축(Compression) 프로그램 구현 및 실험
+// 2020203066 김정언
+
 #pragma once
+#define _CRT_SECURE_NO_WARNINGS
+
 #include <iostream>
+#include <cstdio>
 #include <cstring>
+#include <fstream>
+
+using namespace std;
+
+# define MAX_LENGTH		1000
+
+int Symbol_Num = 69;	// 등장한 Symbol 종류 개수
+int Symbol_frequency[69] = { 0 };	// 등장한 Symbol 종류별 빈도수
+// 0~25 : 대문자 알파벳
+// 26~51 : 소문자 알파벳
+// 52~61 : 숫자
+// 62 : 스페이스(" "), 63 : 쉼표(","), 64 : 마침표("."), 65 : 느낌표("!"),
+// 66 : 물음표("?"), 67 : 하이픈("-"), 68 : enterkey(CR)
+char Symbol[69] = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',	// Symbol
+					'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
+					'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd',
+					'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+					'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
+					'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7',
+					'8', '9', ' ', ',', '.', '!', '?', '-', 'CR' };
 
 class Node {
 public:
-	int weight;
-	int* nxt_ptr;
-	int* prev_ptr;
+	Node();		// 생성자
+	~Node();	// 소멸자
+	
+	void SetNode(char symbol, int frequency, Node* left, Node* right);
+	void ShowNode();
+
+private:
+	char symbol = NULL;
+	int frequency = 0;
+	Node* left = 0;
+	Node* right = 0;
 };
 
-// 입력 받은 문자열의 문자 빈도수를 반환하는 함수
-void frequency(char input[10000]) {
+Node::Node() { }	// 생성자
+Node::~Node() { }	// 소멸자
 
+void Node::SetNode(char symbol, int frequency, Node* left, Node* right) {
+	this->symbol = symbol;
+	this->frequency = frequency;
+	this->left = left;
+	this->right = right;
 }
 
+void Node::ShowNode() {
+	printf("symbol: %c\tfrequency: %d\n", this->symbol, this->frequency);
+}
 
-// 빈도수를 내림차순으로 바꾸는 함수
+// 문자 빈도수를 반환하는 함수
+void frequency(char arr[MAX_LENGTH], int enterkey) {
+	int i = 0;
+
+	while (arr[i] != NULL) {
+		if (arr[i] >= 97 && arr[i] <= 122) {	// 소문자라면 0~25
+			Symbol_frequency[arr[i] - 71]++;
+		}
+		else if (arr[i] >= 65 && arr[i] <= 90) {	// 대문자라면 26~51
+			Symbol_frequency[arr[i] - 65]++;
+		}
+		else if (arr[i] == 63) {	// 물음표라면 66
+			Symbol_frequency[arr[i] + 3]++;
+		}
+		else if (arr[i] >= 48 && arr[i] <= 57) {	// 숫자라면 52~61
+			Symbol_frequency[arr[i] + 4]++;
+		}
+		else if (arr[i] == 46) {	// 마침표라면 64
+			Symbol_frequency[arr[i] + 18]++;
+		}
+		else if (arr[i] == 45) {	// 하이픈이라면 67
+			Symbol_frequency[arr[i] + 22]++;
+		}
+		else if (arr[i] == 44) {	// 쉼표라면 63
+			Symbol_frequency[arr[i] + 19]++;
+		}
+		else if (arr[i] == 33) {	// 느낌표라면 65
+			Symbol_frequency[arr[i] + 32]++;
+		}
+		else if (arr[i] == 32) {	// 스페이스라면 62
+			Symbol_frequency[arr[i] + 30]++;
+		}
+		i++;
+	}
+
+	// enterkey 라면 68
+	Symbol_frequency[68] = enterkey;
+}
+
+// 문자 개수를 세는 함수
+void CountSymbol() {
+	for (int i = 0; i < 69; i++) {
+		if (Symbol_frequency[i] == 0) {
+			Symbol_Num--;
+		}
+	}
+}
--- main.cpp
+++ main.cpp
@@ -1,13 +1,58 @@
-#include "class.h"
+// 2023 알고리즘(박병준 교수님)
+// 과제명 : Huffman 코드를 이용한 파일압축(Compression) 프로그램 구현 및 실험
+// 2020203066 김정언
 
+#include "class.h"
 using namespace std;
 
 int main(void) {
-	char input[10000];
+	FILE* inputFile = fopen("test.txt", "r");	// 입력 파일 읽기 모드로 염
+	int enterkey = -1;	// enterkey 개수를 세는 변수
 
-	printf("입력(영어, 특수문자만 입력해주세요.) >> ");
-	cin >> input;
-	cout << input;
+	// 파일이 없을 경우 프로그램 종료
+	if (inputFile == NULL) {
+		printf("파일이 없습니다. 프로그램을 종료합니다.\n");
+		return 0;
+	}
 
+	// 파일을 한 줄씩 읽어옴
+	while (1) {
+		char arr[MAX_LENGTH];
+		char* ptr = fgets(arr, MAX_LENGTH, inputFile);
+
+		// 파일 끝
+		if (ptr == NULL) {
+			break;
+		}
+
+		enterkey++;
+		frequency(arr, enterkey);
+	}
+
+	fclose(inputFile);
+	CountSymbol();
+
+	Node* node = new Node[Symbol_Num];	// node 배열 생성
+	int n = 0;	// node 배열에 하나씩 저장하기 위한 변수
+
+	for (int i = 0; i < 69; i++) {
+		int tmp = 0;
+
+		for (int j = 0; j < 69; j++) {
+			if (Symbol_frequency[j] == 0) {	// 한번도 나오지 않은 문자일 때
+				continue;
+			}
+
+			if (Symbol_frequency[tmp] < Symbol_frequency[i]) { // 가장 빈도수 큰 위치를 찾음
+				tmp = j;
+			}
+		}
+
+		if (tmp != 0) {	// 노드 생성
+			node[n].SetNode(Symbol[tmp], Symbol_frequency[tmp], 0, 0);
+		}
+	}
+
+	delete[] node;
 	return 0;
 }
